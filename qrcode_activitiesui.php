c6082322d2c9bd631c7cc94af0c5a9309216ae35
organic116 <54708493+organic116@users.noreply.github.com>
2019-09-10 16:37:32 +0700

    Create qrcode_activitiesui.php

---
+++ qrcode_activitiesui.php
@@ -0,0 +1,72 @@
+<?php
+session_save_path("session/");
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <title>ระบบเข้าร่วมกิจกรรมนักศึกษา</title>
+
+    <!-- Bootstrap CSS CDN -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <!-- Our Custom CSS -->
+    <link rel="stylesheet" href="style4.css">
+
+    <!-- Font Awesome JS -->
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
+
+</head>
+
+<body>
+
+    <div class="wrapper">
+        <!-- Sidebar  -->
+
+
+        <!-- Page Content  -->
+								<div id="content">
+									<div align="center">
+										<h2>สแกน QR-CODE เพื่อเข้าร่วมกิจกรรม</h2>
+									</div>
+
+<form align="center" action="adduser_toar.php"  method="post">
+<div class="form-row" >
+	<div class="form-group col-md-12">
+	<input type = "hidden" name = "activities_join" value = "<?php echo $_SESSION["activities_id"];?>">
+		<B>รหัสกิจกรรม :</B><?php echo $_SESSION["activities_id"];?>
+                        &nbsp;&nbsp;<BR><B>ชื่อกิจกรรม:</B><?php echo $_SESSION["activities_name"];?>
+
+<center>
+            <BR>กรุณากรอกรหัสนักศึกษา
+<input type="number" style="width:40% !important;"class="form-control" name="user_id" id="user_id" >
+<button type="submit" name="submit" id="submit" class="btn btn-primary">เข้าร่วมกิจกรรม</button>
+	</center>
+</form>
+
+	</div>
+</div>
+
+    <!-- jQuery CDN - Slim version (=without AJAX) -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <!-- Popper.JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+    <!-- Bootstrap JS -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $('#sidebarCollapse').on('click', function () {
+                $('#sidebar').toggleClass('active');
+            });
+        });
+    </script>
+</body>
+
+
+</html>
