fab1a299bf1f35f6e310a43c60f2a71eb8ab9663
organic116 <54708493+organic116@users.noreply.github.com>
2019-09-10 16:10:11 +0700

    Create db.inc.php

---
+++ db.inc.php
@@ -0,0 +1,4 @@
+<?php
+$con = mysqli_connect('localhost', 'id10717672_organic', 'Tanakorn1340', 'id10717672_dbactivities'); or die("Error: " . mysqli_error($con));
+mysqli_query($con, "SET NAMES 'utf8' ");
+?>
