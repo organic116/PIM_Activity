58fce18083a79c3b91b42957360984218b2a30d8
organic116 <54708493+organic116@users.noreply.github.com>
2019-09-10 16:07:03 +0700

    Create ar_searchlast.php

---
+++ ar_searchlast.php
@@ -0,0 +1,381 @@
+<?php
+session_save_path("session/");
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <title>ระบบเข้าร่วมกิจกรรมนักศึกษา</title>
+
+    <!-- Bootstrap CSS CDN -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <!-- Our Custom CSS -->
+     <!--<link rel="stylesheet" href="style4.css">-->
+	<link rel="stylesheet" href="style4.css?v=<?php echo filemtime('style4.css'); ?>" />
+	<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script> <!-- Css ปุ่ม-->
+    <!-- Font Awesome JS -->
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
+    <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
+	<style type="text/css">
+@import 'https://fonts.googleapis.com/css?family=Ubuntu:300, 400, 500, 700';
+
+*, *:after, *:before {
+  margin: 0;
+  padding: 0;
+}
+
+.svg-container {
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  z-index: -1;
+}
+
+#svg {
+  path {
+    transition: .1s;
+  }
+
+  &:hover path {
+    d: path("M 800 300 Q 400 250 0 300 L 0 0 L 800 0 L 800 300 Z");
+  }
+}
+
+body {
+  background: #fff;
+  color: #333;
+  font-family: 'Ubuntu', sans-serif;
+  position: relative;
+}
+
+h3 {
+  font-weight: 400;
+}
+
+header {
+height:4vw;
+  color: #fff;
+  padding-top: 0vw;
+  padding-bottom: 0vw;
+  text-align: center;
+}
+#rcorners2 {
+    border-radius: 500px;
+    border: 2px solid #3366FF;
+    padding: 20px;
+    width: 200px;
+    height: 150px;
+}
+</style>
+</head>
+
+<body>
+   <div class="wrapper">
+		<nav id="sidebar">
+		<div class="sidebar-header">
+				<h3><center><b>Activities</b></center></h3>
+					<strong>AT</strong>
+				</div>
+					<ul class="list-unstyled components">
+					<li >
+							<a href="admin_adduser.php" >
+								&nbsp;<i  class="fas fa-plus"></i>
+									เพิ่มนักศึกษา
+							</a>
+						</li>
+						<li>
+							<a href="admin_activesuimain.php" >
+								&nbsp;<i class="fas fa-calendar-plus"></i>
+									เพิ่มกิจกรรม
+							</a>
+						</li>
+						<li>
+							<a href="admin_uishowar.php">
+								&nbsp;<i class="fas fa-calendar-alt" ></i>
+									กิจกรรมปัจจุบัน
+							</a>
+						</li>
+						<li class="active">
+							<a href="admin_uishowarlast.php">
+								&nbsp;<i class="fas fa-calendar-check"></i>
+									กิจกรรมที่ผ่านมาแล้ว
+							</a>
+						</li>
+						<li>
+							<a href="admin_arreport.php">
+								&nbsp;<i class="fas fa-print"></i>
+									พิมพ์รายงานกิจกรรมรายปี
+							</a>
+						</li>
+						<li>
+							<a href="admin_showuser.php">
+								&nbsp;<i class="fas fa-address-card" ></i>
+									นักศึกษาทั้งหมด
+							</a>
+						</li>
+						<li>
+							<a href="logout_admin.php" onclick="return confirm('คุณต้องการออกจากระบบใช่หรือไม่ ?')">
+								&nbsp;<i class="fas fa-sign-out-alt"></i>
+									ออกจากระบบ
+							</a>
+						</li>
+					</ul>
+<button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+<i class="fas fa-align-justify"></i>
+</button>
+		</nav>
+
+
+
+        <!-- Page Content  -->
+        <div id="content">
+	<div class="svg-container">
+    <svg viewbox ="0 0 800 43" class="svg">
+      <path id="curve" fill="#00aae1" d="M 800 80 Q 400 350 0 300 L 0 0 L 800 0 L 800 300 Z">
+      </path>
+    </svg>
+  </div>
+
+  <header>
+    <h2>ระบบเข้าร่วมกิจกรรมนักศึกษา</h2>
+  </header>
+  <br>
+             <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container-fluid">
+
+                <button type="button" id="sidebarCollapse" class="btn btn-info">
+                        <i class="fas fa-align-right"></i></button>&nbsp;&nbsp;&nbsp;&nbsp;
+
+                    <h4>ค้นหากิจกรรมในอดีต</h4>
+                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="nav navbar-nav ml-auto">
+                        <B>ผู้ใช้งาน :&nbsp;</B><?php echo $_SESSION["personal_name"];?>
+                        &nbsp;<B>สถานะ :&nbsp;</B><?php
+
+									$a=$_SESSION["major_id"];
+													if ($a==1)
+													echo "เจ้าหน้าที่";
+													else($a=="")
+			                        ?>
+                        </ul>
+                </div>
+            </div>
+            </nav>
+			<script language="javascript">
+					function fncSubmit()
+						{
+						if(document.frmfind.name_ar.value == "" && document.frmfind.name_selar.value == ""&& document.frmfind.name_month.value == "")
+							{
+							swal("เกิดข้อผิดพลาด!", "กรุณากรอกข้อมูล", "error");
+							return false;
+							}
+						document.frmfind.submit();
+						}
+						</script>
+	<table width="100%" cellpadding="4" cellspacing="0" ><tr id="rcorners2">
+<td>
+<br>
+			<form class="form-inline" name="frmfind" method="get"  onSubmit="JavaScript:return fncSubmit();" action="ar_searchlast.php">
+	&nbsp;&nbsp;&nbsp;&nbsp;ชื่อกิจกรรม&nbsp;
+		<input class="form-control mr-sm-2" type="text" name="name_ar"   placeholder="กรุณากรอกชื่อกิจกรรม" aria-label="Search"  />
+<div class="form-group col-md-2.5">
+     <div class="input-group-prepend">
+
+     <label for="name_selar">กลุ่มกิจกรรม &nbsp;</label>
+  </div>
+  <select class="custom-select" name="name_selar">
+    <option value="" selected>กรุณาเลือก</option>
+    <option value="1">กิจกรรมบังคับ </option>
+	<option value="2">กิจกรรมเลือก </option>
+	<option value="3">กิจกรรมอื่นๆ </option>
+  </select>
+ </div>
+ <div class="form-group col-md-2.5">
+     <div class="input-group-prepend">
+     <label for="name_month">&nbsp;เดือนที่จัดกิจกรรม &nbsp;</label>
+  </div>
+  <select class="custom-select" name="name_month">
+    <option value="" selected>กรุณาเลือก</option>
+    <option value="-01-">มกราคม </option>
+	<option value="-02-">กุมภาพันธ์ </option>
+	<option value="-03-">มีนาคม </option>
+	 <option value="-04-">เมษายน </option>
+	<option value="-05-">พฤษภาคม </option>
+	<option value="-06-">มิถุนายน </option>
+	 <option value="-07-">กรกฎาคม </option>
+	<option value="-08-">สิงหาคม </option>
+	<option value="-09-">กันยายน </option>
+	 <option value="-10-">ตุลาคม </option>
+	<option value="-11-">พฤศจิกายน </option>
+	<option value="-12-">ธันวาคม </option>
+  </select>
+ </div>
+  &nbsp;&nbsp;&nbsp;&nbsp;<button id='sidebarCollapse' class='btn btn-info' type="submit" name="find_Emp">
+			 <i class='fas fa-search'></i></button>
+</form>
+
+		<br>
+		<a href='admin_uishowarlast.php'>
+			   &nbsp;&nbsp;&nbsp;&nbsp;<button type='button' id='sidebarCollapse' class='btn btn-info'>
+			 <i class="fas fa-home"></i>&nbsp;กลับหน้าแสดงกิจกรรม
+					</button></a>
+		<br>
+		</br>
+		<?php
+function DateThai($strDate)
+{
+$strYear = date("Y",strtotime($strDate))+543;
+$strMonth= date("n",strtotime($strDate));
+$strDay= date("j",strtotime($strDate));
+$strMonthCut = Array("","มกราคม","กุมภาพันธ์","มีนาคม","เมษายน","พฤษภาคม","มิถุนายน","กรกฎาคม","สิงหาคม","กันยายน","ตุลาคม","พฤศจิกายน","ธันวาคม");
+$strMonthThai=$strMonthCut[$strMonth];
+return "$strDay $strMonthThai $strYear";
+}
+
+$strDate =date("d-m-Y");
+?>
+       <?php
+	   $fitdate = date("Y-m-d");
+$SQL="SELECT * FROM tb_activities  WHERE activities_startdate < '".$fitdate."'   AND activities_startdate  ";
+	if(!$_GET["name_ar"] == ""){
+		$SQL .= "AND activities_name LIKE '%".$_GET["name_ar"]."%' ORDER BY activities_addstart DESC";
+		//$SQL .= "WHERE activities_startdate >= '".$fitdate."'   AND activities_startdate";
+	}
+	if(!$_GET["name_selar"] == ""){
+		$SQL .= "AND genus_id  = '".$_GET["name_selar"]."' ORDER BY activities_addstart DESC";
+		//	$SQL .= "WHERE activities_startdate >= '".$fitdate."'   AND activities_startdate";
+	}
+	if(!$_GET["name_month"] == ""){
+		$SQL .= "AND activities_startdate LIKE '%".$_GET["name_month"]."%' ORDER BY activities_addstart DESC";
+	//	$SQL .= "WHERE activities_startdate >= '".$fitdate."'   AND activities_startdate";
+
+	}
+	$server=$_SERVER['localhost'];
+  $username ="id10717672_organic";
+  $password ="Tanakorn1340";
+  $db ="id10717672_dbactivities";
+	$conn=mysqli_connect($server,$username,$password,$database);
+
+if(!$conn){
+die("เกิดข้อผิดพลาดในการเชื่อมต่อกับเครื่องให้บริการฐานข้อมูล!!!!!".mysqli_connect_error());
+}
+mysqli_set_charset($conn,"utf8");
+$result=mysqli_query($conn,$SQL) or die("เกิดข้อผิดพลาดในการค้นหา").mysqli_error();
+if(mysqli_num_rows($result)==1){
+$row=mysqli_fetch_array($result);
+$query0 = "SELECT COUNT(activities_id) AS countjoin FROM tb_joinac WHERE activities_id = '".$row["activities_id"]."'" or die(mysql_error());
+$result0 = mysqli_query($conn,$query0);
+$rowc = mysqli_fetch_array($result0,MYSQLI_ASSOC);
+$counts = $rowc["countjoin"];
+$diss = "";
+	  if($counts != 0)
+	  {
+			$diss = "disabled";
+	  }
+echo"<div class='col-lg-12'>";
+echo"<table  border='2' align='center' class='form-group table table-striped' width='100px' style='table-layout:fixed' >";
+echo"<tr align='center' bgcolor='#DCDCDC'>";
+echo"<td width='100px' bgcolor='#00aae1'><B>ชื่อกิจกรรม</B></td>";
+echo"<td width='100px' bgcolor='#00aae1'><B>วันเริ่มกิจกรรม</B></td>";
+echo"<td width='100px' bgcolor='#00aae1'><B>เวลาเริ่มกิจกรรม</B></td>";
+echo"<td width='30px' bgcolor='#00aae1'><B>ชั่วโมง</B></td>";
+echo"<td width='80px' bgcolor='#00aae1'><B>จำนวนผู้เข้าร่วม</B></td>";
+echo"<td width='100px' bgcolor='#00aae1'><B>จัดการข้อมูล</B></td>";
+echo"</tr>";
+echo "<tr align='center' bgcolor='#FFFFF0'>";
+echo "<td>$row[activities_name]</td>";
+echo "<td>" .'<center>'.DateThai ($row["activities_startdate"])."</td> ";
+echo "<td>".date('H:i',strtotime($row["activities_starttime"]))."&nbsp;น. - ".date('H:i',strtotime($row["activities_endtime"]))."&nbsp;น.</td> ";
+echo "<td>" .'<center>'.$row["activities_hour"] .  "</td> ";
+echo "<td>" .'<center>'.$counts.'/'.$row["activities_valueuser"] .  "&nbsp;คน</td> ";
+  echo "<td>
+			<a href='join_ar.php?ar_id=".$row["activities_id"]."'>
+			  <button type='button' id='sidebarCollapse' class='btn btn-success'>
+			 <i class='fas fa-info-circle'></i></button></a>
+
+		</td>";
+      echo "</tr>";
+
+}else if (mysqli_num_rows($result)>1){
+echo"<div class='col-lg-12'>";
+echo"<table  border='2' align='center' class='form-group table table-striped' width='100px' style='table-layout:fixed' >";
+echo"<tr align='center'>";
+echo"<td width='100px' bgcolor='#00aae1'><B>ชื่อกิจกรรม</B></td>";
+echo"<td width='100px' bgcolor='#00aae1'><B>วันเริ่มกิจกรรม</B></td>";
+echo"<td width='100px' bgcolor='#00aae1'><B>เวลาเริ่มกิจกรรม</B></td>";
+echo"<td width='30px' bgcolor='#00aae1'><B>ชั่วโมง</B></td>";
+echo"<td width='80px' bgcolor='#00aae1'><B>จำนวนผู้เข้าร่วม</B></td>";
+echo"<td width='100px' bgcolor='#00aae1'><B>จัดการข้อมูล</B></td>";
+echo"</tr>";
+$con = mysqli_connect('localhost', 'id10717672_organic', 'Tanakorn1340', 'id10717672_dbactivities');
+mysqli_set_charset($con,"utf8");
+$queryz = mysqli_query($con, $SQL) or die("เกิดข้อผิดพลาดในการค้นหา").mysqli_error();;
+while ($rowz = mysqli_fetch_assoc($queryz)){
+$query0 = "SELECT COUNT(activities_id) AS countjoin FROM tb_joinac WHERE activities_id = '".$rowz["activities_id"]."'" or die(mysql_error());
+$result0 = mysqli_query($conn,$query0);
+$rowc = mysqli_fetch_array($result0,MYSQLI_ASSOC);
+$countz = $rowc["countjoin"];
+$disz = "";
+	  if($countz != 0)
+	  {
+			$disz = "disabled";
+	  }
+echo "<tr align='center' bgcolor='#FFFFF0'>";
+echo "<td>$rowz[activities_name]</td>";
+echo "<td>" .'<center>'.DateThai ($rowz["activities_startdate"])."</td> ";
+echo "<td>".date('H:i',strtotime($rowz["activities_starttime"]))."&nbsp;น. - ".date('H:i',strtotime($rowz["activities_endtime"]))."&nbsp;น.</td> ";
+echo "<td>" .'<center>'.$rowz["activities_hour"] .  "</td> ";
+echo "<td>" .'<center>'.$countz.'/'.$rowz["activities_valueuser"] .  "&nbsp;คน</td> ";
+  echo "<td>
+			<a href='join_ar.php?ar_id=".$rowz["activities_id"]."'>
+			  <button type='button' id='sidebarCollapse' class='btn btn-success'>
+			 <i class='fas fa-info-circle'></i></button></a>
+
+		</td>";
+      echo "</tr>";
+
+}
+echo "</table>";
+echo "</div>";
+
+
+}else{
+	echo "ไม่พบข้อมูลกิจกรรมที่ต้องการค้นหา";
+	exit(0);
+	}
+	mysqli_close($conn);
+
+
+ ?>
+ </td></tr><br></table>
+ <!-- jQuery CDN - Slim version (=without AJAX) -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <!-- Popper.JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+    <!-- Bootstrap JS -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $('#sidebarCollapse').on('click', function () {
+                $('#sidebar').toggleClass('active');
+            });
+        });
+    </script>
+    <!-- นำเข้า Javascript jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.2/js/select2.min.js"></script>
+
+</body>
+
+</html>
