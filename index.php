6efc6d3fa4d183f83d1d3d917567b3d77d6f0bc8
organic116 <54708493+organic116@users.noreply.github.com>
2019-09-10 16:34:13 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,175 @@
+<!doctype html>
+<html lang="en">
+ <head>
+  <meta charset="UTF-8">
+  <meta name="Generator" content="EditPlus®">
+  <meta name="Author" content="">
+  <meta name="Keywords" content="">
+  <meta name="Description" content="">
+  <link href="https://fonts.googleapis.com/css?family=Kanit" rel="stylesheet">
+  <link rel="stylesheet" href="css.css" type="text/css" />
+  <link rel ="stylesheet" type="text/css" href = "css/style.css">
+  <title>ระบบเข้าร่วมกิจกรรมของนักศึกษา</title>
+  <STYLE TYPE="TEXT/CSS"><!--
+A:link	{
+text-decoration:none;
+}
+A:visited	{
+text-decoration:none;
+}
+//--></STYLE>
+  <style type="text/css">
+		:root {
+  --input-padding-x: 1.5rem;
+  --input-padding-y: .75rem;
+}
+
+body {
+  background: #9CECFB;
+  /* fallback for old browsers */
+  background: -webkit-linear-gradient(to right, #0052D4, #65C7F7, #9CECFB);
+  /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(to right, #0052D4, #65C7F7, #9CECFB);
+  /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+}
+
+.card-signin {
+  border: 0;
+  border-radius: 1rem;
+  box-shadow: 0 0.5rem 1rem 0 rgba(0, 0, 0, 0.1);
+}
+
+.card-signin .card-title {
+  margin-bottom: 2rem;
+  font-weight: 300;
+  font-size: 1.5rem;
+}
+
+.card-signin .card-body {
+  padding: 2rem;
+}
+
+.form-signin {
+  width: 100%;
+}
+
+.form-signin .btn {
+  font-size: 80%;
+  border-radius: 5rem;
+  letter-spacing: .1rem;
+  font-weight: bold;
+  padding: 1rem;
+  transition: all 0.2s;
+}
+
+.form-label-group {
+  position: relative;
+  margin-bottom: 1rem;
+}
+
+.form-label-group input {
+  border-radius: 2rem;
+}
+
+.form-label-group>input,
+.form-label-group>label {
+  padding: var(--input-padding-y) var(--input-padding-x);
+}
+
+.form-label-group>label {
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: block;
+  width: 100%;
+  margin-bottom: 0;
+  /* Override default `<label>` margin */
+  line-height: 1.5;
+  color: #495057;
+  border: 1px solid transparent;
+  border-radius: .25rem;
+  transition: all .1s ease-in-out;
+}
+
+.form-label-group input::-webkit-input-placeholder {
+  color: transparent;
+}
+
+.form-label-group input:-ms-input-placeholder {
+  color: transparent;
+}
+
+.form-label-group input::-ms-input-placeholder {
+  color: transparent;
+}
+
+.form-label-group input::-moz-placeholder {
+  color: transparent;
+}
+
+.form-label-group input::placeholder {
+  color: transparent;
+}
+
+.form-label-group input:not(:placeholder-shown) {
+  padding-top: calc(var(--input-padding-y) + var(--input-padding-y) * (2 / 3));
+  padding-bottom: calc(var(--input-padding-y) / 3);
+}
+
+.form-label-group input:not(:placeholder-shown)~label {
+  padding-top: calc(var(--input-padding-y) / 3);
+  padding-bottom: calc(var(--input-padding-y) / 3);
+  font-size: 12px;
+  color: #777;
+}
+
+.btn-google {
+  color: white;
+  background-color: #ea4335;
+}
+
+.btn-facebook {
+  color: white;
+  background-color: #3b5998;
+}
+	img.avatar {
+    width: 40%;
+    border-radius: 50%;
+}
+	.imgcontainer {
+    text-align: center;
+    margin: 24px 0 12px 0;
+    position: relative;
+}
+
+
+  </style>
+ </head>
+ <body style="padding:90px 0px;">
+ <center>
+ <div class= "text">
+  <table style ="font-family: 'Kanit', sans-serif;" width = "500">
+  <tr>
+  <td colspan="3"><center><img src="img/icon_menu.png" height="150"><br><font size="6">ระบบเข้าร่วมกิจกรรมของนักศึกษา</font><br><hr></center></td>
+  </tr>
+  <tr>
+  	<td>
+  		<center><a href="login.php"><img src="img/icon_student.png" height="100"><br><h3>นักศึกษา</h3></center></a></td>
+  	<td>
+  		<center><a href="login_teacher.php"><img src="img/icon_teacher.png" height="100"><br><h3>อาจารย์</h3></center></a></td>
+  	<td>
+  		<center><a href="login_admin.php"><img src="img/user01.png" height="100"><br><h3>เจ้าหน้าที่</h3></center></a></td>
+  </tr>
+  </table>
+</div>
+  </center>
+  <script src="assets/js/vendor/jquery-1.11.2.min.js"></script>
+        <script src="assets/js/vendor/bootstrap.min.js"></script>
+
+        <script src="assets/js/jquery-easing/jquery.easing.1.3.js"></script>
+        <script src="assets/js/wow/wow.min.js"></script>
+        <script src="assets/js/plugins.js"></script>
+        <script src="assets/js/main.js"></script>
+ </body>
+ 
+</html>
